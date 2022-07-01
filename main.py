7db1b812ec1b2b970b5a6add6c63ec5fe3586a1c
denizOzgur <denizozgur@users.noreply.github.com>
2022-07-01 16:02:44 -0700

    init

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from BST import BinarySearchTree as BST
+import random
+
+from BST import BinarySearchTree as Bst
 from BST import Node as Node
 
 
 def main():
-	mytree = BST()
+	my_tree = Bst()
 	nums = [12, 6, 18, 19, 21, 11, 6, 3, 5, 4, 24, 18]
+	nums.extend([random.randint(0, 400) for x in range(100)])
 	for num in nums:
-		mytree.insert(Node(num))
+		my_tree.insert(Node(num))
 
-	mytree.printInorder()
+	my_tree.printInorder()
 
 
 if __name__ == '__main__':
